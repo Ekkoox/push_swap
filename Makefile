bf47900bc96035ad9d031c8e8b21cd4757b3b64f
Enzo Schneider <enschnei@bess-f4r2s2.clusters.42paris.fr>
2024-02-23 12:38:54 +0100

    start

---
+++ Makefile
@@ -0,0 +1,28 @@
+NAME = push_swap
+
+C_FILES = main.c\
+
+SRCS = $(addprefix srcs/,$(C_FILES))
+
+CFLAGS = -Wall -Werror -Wextra -I includes/
+
+LIB = ft_printf/libftprintf.a libft/libft.a
+DEPS = $(addprefix lib/,$(LIB))
+
+all: ${NAME}
+
+${NAME}: ${SRCS}
+	make -C lib/libft
+	make -C lib/ft_printf
+	cc ${CFLAGS} ${SRCS} -I ${NAME} ${DEPS}
+
+clean:
+	make -C lib/libft clean
+	make -C lib/ft_printf clean
+
+fclean:	clean
+		rm -rf ${NAME}
+
+re:             fclean all
+
+.PHONY:         all clean fclean re
---
+++ includes/push_swap.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enschnei <enschnei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/23 12:28:16 by enschnei          #+#    #+#             */
+/*   Updated: 2024/02/23 12:28:45 by enschnei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "../lib/ft_printf/ft_printf.h"
+# include "../lib/libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+
+
+#endif
